4e100db95ea4c099194d854019eb34afffc1a4d3
NSD <metall112@bk.ru>
2016-12-14 15:10:51 +0200

    амы

--- src/main/java/database/DB.java
+++ src/main/java/database/DB.java
@@ -9,8 +9,8 @@ public abstract class DB {
 
     protected static final String databaseUrl = "jdbc:mysql://127.2.159.2:3306/doctrade";
     //private static final String databaseUrl = "jdbc:mysql://localhost:3307/tochka";
-    protected static final String userName = "adminHaMdl7W";
-    protected static final String password = "t--T669ptLfE";
+    protected static final String userName = "adminsBmIZAN";
+    protected static final String password = "qIqWymbbb-hk";
     protected Connection conn = null;
 
 }
