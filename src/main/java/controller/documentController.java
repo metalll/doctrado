d2680a63b77fdba9c453f2e611018e2551890e32
Леша <metall112@bk.ru>
2017-01-22 00:44:25 +0200

    Rubicon Review registrared test upload photo
    Signed-off-by: Леша <metall112@bk.ru>

--- src/main/java/controller/documentController.java
+++ src/main/java/controller/documentController.java
@@ -10,10 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.List;
 
 /**
@@ -88,11 +85,23 @@ public class documentController extends HttpServlet {
                         }
                         String str = result.toString("UTF-8");
 
-                        response.setContentType("text/html; charset=UTF-8");
-                        PrintWriter out = response.getWriter();
-                        out.write(str + " has worked");
-                        out.flush();
-                        out.close();
+                        BufferedWriter writer = null;
+                        try {
+
+                            writer = new BufferedWriter(new FileWriter("file"));
+
+
+                            writer.write(str);
+                        } catch (IOException e) {
+                        } finally {
+                            try {
+                                if (writer != null)
+                                    writer.close();
+                            } catch (IOException e) {
+                            }
+                        }
+
+
                         continue;
                     }
 
