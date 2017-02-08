3d4ab3d778f550f2a170f17336b26613604ecf20
Леша <metall112@bk.ru>
2017-02-08 11:05:56 +0200

    Rubicon Review develop full view/edit course + subcourses
    Signed-off-by: Леша <metall112@bk.ru>

--- src/main/java/controller/getCourse.java
+++ src/main/java/controller/getCourse.java
@@ -17,6 +17,7 @@ public class getCourse extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html;charset=UTF-8");
+        request.getRequestDispatcher("./courseVC.jsp").include(request, response);//рис
     }
 }
